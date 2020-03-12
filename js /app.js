12327d0df1f5a9fa6763383c28fcff8891b4c03b
Your Name <you@example.com>
2020-03-12 01:53:47 -0400

    fix: drop menu function was incorrectly labeled. Main-content is visible

--- js /app.js
+++ js /app.js
@@ -1,60 +1,50 @@
-$(function(){
-  
-    $("#select").on("change", function() {
-        let userInput = $("#select").val();
-    console.log(userInput);
+$(function() {
+  console.log("working");
+  $(".dropbtn").on("change", function() {
+    console.log("function");
+    let userInput = $("select").val();
+    // console.log(userInput);
+
     $.ajax({
-        dataType: "json",
-        method: "GET",
-        url: `https://api.nytimes.com/svc/topstories/v2/${userInput}.json?api-key=qZeF0WlVIE49aaMuTGpSM5bAxYeDLSyT`,
+      dataType: "json",
+      method: "GET",
+      url: `https://api.nytimes.com/svc/topstories/v2/${userInput}.json?api-key=qZeF0WlVIE49aaMuTGpSM5bAxYeDLSyT`
+    }).done(function({ results }) {
+      $.each(results, function(index, value) {
+        // Max-listing
+        if (index === 12) return false;
 
-            }).done(function ({results}){
-                 //Max listing
-    
-                // console.log(results)
-                $.each(results, function(index, value) {
-                    if (index === 12) return false;
-                    // console.log (index,value)
-                    console.log (value)
-                    // console.log (userInput)
+        // console.log(results);
+        // console.log(value);
+        // console.log(userInput);
 
+        // ! Check your work !
+        // hasOwnProperty returns a boolean value indicating
+        //  whether the object has a property with the name of the argument.
+        // if (value.multimedia) {
+        //   let length = value.multimedia.length;
+        //   //   console.log("length: ", length);
+        // }
 
-                     
-                    // hasOwnProperty returns a boolean value indicating whether the object has a property with the name of the argument. 
-                if(value.multimedia){
-                    let length = value.multimedia.length
-                    console.log("length: ", length)
-                }
+        //Variables
+        let articleImage = value.multimedia[0].url;
+        let url = value.url;
+        let description = value.abstract;
 
-                    //Variables
-                    let articleImage = value.multimedia[0].url
-                    let url = value.url
-                    let description = value.abstract
-              
-                    // console.log(articleImage)
-                    // console.log(url)
-                    // console.log(description);
-               
-                   
-                   
-                   
+        console.log(articleImage);
+        console.log(url);
+        console.log(description);
 
-                    //Display articles 
-
-                    $('.main-content').append(`
-                    <figure class="articleImage">
-                        <img src="${articleImage}">
-                        <a href="${url}" target="new">
-                            <p class="article-text">${description}<p>
-                        </a>
-                    </figure>`);
-
-
-                          
-                
-            })
-        })
-
-  
-      })
+        //Display articles
+        $(".main-content").append(`
+                <figure class="article-image">
+                    <img src="${articleImage}">
+                    <a href="${url}" target="new"><p class="article-text">${description}</p></a>
+                </figure>`);
+      });
+      $(".dropbtn").on("change", function() {
+        $(".main-content").empty();
+      });
+    });
+  });
 });
