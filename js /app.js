5b7f6f5f99c6cf0e70e518e6a3b337d84d694fa9
Your Name <you@example.com>
2020-03-12 09:08:18 -0400

    chore: remove extraneous code

--- js /app.js
+++ js /app.js
@@ -1,9 +1,6 @@
 $(function() {
-  console.log("working");
   $(".dropbtn").on("change", function() {
-    console.log("function");
     let userInput = $("select").val();
-    // console.log(userInput);
 
     $.ajax({
       dataType: "json",
@@ -14,26 +11,10 @@ $(function() {
         // Max-listing
         if (index === 12) return false;
 
-        // console.log(results);
-        // console.log(value);
-        // console.log(userInput);
-
-        // ! Check your work !
-        // hasOwnProperty returns a boolean value indicating
-        //  whether the object has a property with the name of the argument.
-        // if (value.multimedia) {
-        //   let length = value.multimedia.length;
-        //   //   console.log("length: ", length);
-        // }
-
         //Variables
         let articleImage = value.multimedia[0].url;
         let url = value.url;
         let description = value.abstract;
-
-        console.log(articleImage);
-        console.log(url);
-        console.log(description);
 
         //Display articles
         $(".main-content").append(`
